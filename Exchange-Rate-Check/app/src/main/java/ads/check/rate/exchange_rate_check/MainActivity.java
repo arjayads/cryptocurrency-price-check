6b1648c7a89cc9e1c4d6f4e276961ea8fda0e075
Arjay <arjayads@gmail.com>
2017-05-06 10:06:52 +0800

    set menu items

--- Exchange-Rate-Check/app/src/main/java/ads/check/rate/exchange_rate_check/MainActivity.java
+++ Exchange-Rate-Check/app/src/main/java/ads/check/rate/exchange_rate_check/MainActivity.java
@@ -17,15 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -43,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_save) {
             return true;
         }
 
