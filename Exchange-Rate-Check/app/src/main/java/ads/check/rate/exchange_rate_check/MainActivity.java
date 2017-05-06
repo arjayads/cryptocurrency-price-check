0a03a0cdbe78ef1c65a6685df4cc241dcb677295
Arjay <arjayads@gmail.com>
2017-05-06 10:42:48 +0800

    add autocompletextviews

--- Exchange-Rate-Check/app/src/main/java/ads/check/rate/exchange_rate_check/MainActivity.java
+++ Exchange-Rate-Check/app/src/main/java/ads/check/rate/exchange_rate_check/MainActivity.java
@@ -1,15 +1,20 @@
 package ads.check.rate.exchange_rate_check;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView baseAutoCompleteTV;
+    AutoCompleteTextView targetAutoCompleteTV;
+
+    String[] arr = { "Paries,France", "PA,United States","Parana,Brazil",
+            "Padua,Italy", "Pasadena,CA,United States"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setBaseAutocompleteData();
     }
+
+    private void setBaseAutocompleteData() {
+        baseAutoCompleteTV = (AutoCompleteTextView)
+                findViewById(R.id.base_autoCompleteTextView);
+
+        ArrayAdapter<String> badapter = new ArrayAdapter<> (this,android.R.layout.select_dialog_item, arr);
+
+        baseAutoCompleteTV.setThreshold(1);
+        baseAutoCompleteTV.setAdapter(badapter);
+
+
+        targetAutoCompleteTV = (AutoCompleteTextView)
+                findViewById(R.id.target_autoCompleteTextView);
+
+        ArrayAdapter<String> tadapter = new ArrayAdapter<> (this,android.R.layout.select_dialog_item, arr);
+
+        targetAutoCompleteTV.setThreshold(1);
+        targetAutoCompleteTV.setAdapter(tadapter);
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
